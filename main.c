2c46994b7e3b92d54bf7d07ca19c9ee2e7344ac9
ojakinkade <ojakinlade@gmail.com>
2024-04-19 16:41:17 +0100

    update

--- main.c
+++ main.c
@@ -10,10 +10,12 @@
 #define MSG_ID_READ_START    (MSG_ID_WRITE_START + XIP_BASE)
 
 
-void Flash_WriteMsgID(uint32_t msg_id)
-{
+void Flash_WriteMsgID(uint8_t msg_id)
+{    
+    uint8_t buf[PAGE_SIZE] = {0};
+    buf[0] = msg_id;
     flash_range_erase(MSG_ID_WRITE_START,NVS_SIZE);
-    flash_range_program(MSG_ID_WRITE_START,(const uint8_t*)msg_id,PAGE_SIZE);
+    flash_range_program(MSG_ID_WRITE_START, buf,PAGE_SIZE);
 }
 
 uint8_t Flash_ReadMsgID(void)
@@ -27,7 +29,8 @@ int main()
     stdio_init_all();
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    Flash_WriteMsgID(1);
+    uint8_t msgID = 50;
+    Flash_WriteMsgID(msgID);
     sleep_ms(500);
     uint8_t id = Flash_ReadMsgID();
 
